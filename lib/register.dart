6d834913d1cfca8f5cf6aff2ef55fc46d1d3d21d
AbdullahShahzad-302 <145151091+Abdullahshaz70@users.noreply.github.com>
2025-10-07 20:15:30 +0500

    Register page ki bunyaad

---
+++ lib/register.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+
+
+class Register extends StatefulWidget {
+  
+
+  @override
+  State<Register> createState() => _Register();
+}
+
+class _Register extends State<Register> {
+
+
+  Widget build(BuildContext){
+    return Scaffold(
+
+    );
+  }
+
+
+}

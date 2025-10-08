40dd4f32d58adceb0628ebe3ca10250a40441bb3
AbdullahShahzad-302 <145151091+Abdullahshaz70@users.noreply.github.com>
2025-10-08 11:41:19 +0500

    Sheeqe ne kaam karna hai

--- lib/register.dart
+++ lib/register.dart
@@ -1,10 +1,11 @@
+import 'dart:ffi';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 
 
 class Register extends StatefulWidget {
-  
 
   @override
   State<Register> createState() => _Register();
@@ -12,36 +13,12 @@ class Register extends StatefulWidget {
 
 class _Register extends State<Register> {
 
+  final _formKey = GlobalKey<FormState>();
 
   Widget build(BuildContext){
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 100,
-        backgroundColor: const Color.fromARGB(255, 2, 52, 94),
-        foregroundColor: Colors.white,
-        title: Column(
-          children: [
-            Center(),
-            Text("Hazir",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold
-              ),
-            ),
-            Text("Bhai Hazir hai!",
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 14,
-              fontWeight: FontWeight.bold
-              ),
-            ),
-            
-
-          ],
-        ),
-      )
-
-    );
+      
+    );  
   }
 
 
